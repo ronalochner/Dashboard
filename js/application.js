b36b8fe5bd7c52a7119f57bca0a38210ee48a305
ronalochner <ronalochner@gmail.com>
2017-09-20 11:21:49 +0200

    Dynamic Forms
    Forms Work Dynamic

--- js/application.js
+++ js/application.js
@@ -1,6 +1,18 @@
-var january = {
-    month = "January",
-    income = "4000",
-    expenses = "1000",
-    hours = "12",
-}
+
+var tabledata = [
+   {month:'January', income:'5400', expenses:'300', hours:'50'},
+   {month:'February', income:'3400', expenses:'300', hours:'100'},
+   {month:'March', income:'2400', expenses:'300', hours:'12'},
+   {month:'April', income:'5700', expenses:'300', hours:'45'},
+]
+
+var app = new Vue ({
+   el:'#app',
+   data:{
+      tabledata:tabledata
+   }
+   
+})
+
+
+
